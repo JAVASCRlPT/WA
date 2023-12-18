3adfc089e177119247ce733b9cacb2f1f49fa3b0
JAVASCRlPT <sahrulsidik000@gmail.com>
2023-12-18 10:00:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const require = createRequire(__dirname) // Bring in the ability to create the 'require' method
 const { name, author } = require(join(__dirname, './package.json')) // https://www.stefanjudis.com/snippets/how-to-import-json-files-in-es-modules-node-js/
 
-say('KAWAINIME TOOLS', { font: 'chrome', align: 'center', gradient: ['blue', 'green'] })
+say('REG DOT TOOLS', { font: 'chrome', align: 'center', gradient: ['blue', 'green'] })
 say(`Whatsapp Bot`, { font: 'console', align: 'center', gradient: ['blue', 'green'] })
 
 console.log(`
-KAWAINIME TOOLS
+REG DOT TOOLS
 `)
 
 var isRunning = false
